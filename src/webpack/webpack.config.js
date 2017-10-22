986642bb6fcd16aba7134e408cbadb672ca11c75
dali <dalabord@hotmail.com>
2017-10-22 11:09:39 +0200

    typescript pojecte : Init & ts-loader added

---
+++ src/webpack/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: "./main.ts",
+    context: __dirname, // identifier tsconfig.json automatiquement
+    output: {
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+        { test: /\.tsx?$/, // ts & tsx gérés
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true 
+            } 
+        },
+    ]}
+};
